bf413b643cbc27eca3015dbdc0dc6f99b0a74eaa
Adeeel98 <mohamed.naguibahmed@student.guc.edu.eg>
2019-08-10 12:41:07 +0200

    admin stories without the cards

---
+++ models/History.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const db = require('../config/database')
+const History = db.define('Employee', {
+  formId: Sequelize.INTEGER,
+  formSubmition: Sequelize.DataTypes.DATE.toString(),
+  distributionSubmition: Sequelize.DataTypes.DATE.toString(),
+  sourcingSubmition: Sequelize.DataTypes.DATE.toString(),
+  fleatSubmition: Sequelize.DataTypes.DATE.toString(),
+  irmrSubmition: Sequelize.DataTypes.DATE.toString(),
+  ciSubmition: Sequelize.DataTypes.DATE.toString() },
+{ timestamps: false,
+  freezeTableName: true
+}
+)
+
+module.exports = History
---
+++ routes/api/histories.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../../controllers/historyController')
+// get all
+router.get('/', controller.default)
+
+// add permission
+router.post('/', controller.create)
+
+// update permission
+router.put('/:id', controller.update)
+// delete permission
+router.delete('/:id', controller.delete)
+module.exports = router
