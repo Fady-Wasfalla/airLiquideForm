d663db31acea71d948a78b8ad899d77b4f08235b
Fady-Wasfalla <fady.wasfalla@gmail.com>
2019-08-10 12:48:21 +0200

    Header and Footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,25 @@ import './App.css';
 import { BrowserRouter as Router, Route , Switch } from "react-router-dom";
 import Header from './components/header'
 import Footer from './components/footer'
+import Adel from './components/adel'
+import adel from './components/adel';
 
 function App() {
   return (
     
     <Router>
-<link
-  rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-  crossorigin="anonymous"
-/>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"/>
         <Route component={Header} />
-        <p>The page of evey user will be loaded here</p>
-          <div className="container">
+        
               <Switch>
+              <Route exact path="/adel" component={adel} />
 
               </Switch>
-          </div>
+         
           <Route component={Footer} />        
 </Router>
     )   
