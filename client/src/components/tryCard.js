213d5eebcce5a0f370dc40b20cbcde342afaa214
Fady-Wasfalla <fady.wasfalla@gmail.com>
2019-08-19 13:32:32 +0200

    DB

--- client/src/components/tryCard.js
+++ client/src/components/tryCard.js
@@ -10,7 +10,11 @@ class tryCard extends Component {
         extremeTemperature:0,
         maximumFlow:0 ,
         volumeStored:0 ,
-        Characteristics:0 ,
+        characteristics:0 ,
+        nature1:"",
+        nature2:"",
+        nature3:"",
+        natureOther:"",
       }
 
    
@@ -18,19 +22,75 @@ class tryCard extends Component {
       render() {
         return (
             <React.Fragment>
+              <Col md={12}>
             <Form>
 
                 <Form.Row>
-                            <Form.Group as={Col} controlId="customerName">
-                            <Form.Label>Customer Name</Form.Label>
+                            <Form.Group as={Col} >
+                            <Form.Label>Fluid / Product</Form.Label>
                             <Form.Control as="textarea" rows="1" 
-                            onChange={(e)=>{this.setState({name:e.target.value})
+                            onChange={(e)=>{this.setState({fluidOrProduct:e.target.value})
                             console.log(e.target.value)}}/>
+                            </Form.Group>
+
+                            <Form.Group as={Col} >
+                            <Form.Label>Extreme pressure (Bar)</Form.Label>
+                            <Form.Control type={"number"} step={0.1}  onChange={(e)=>{this.setState({extremePressure:e.target.value})}} />
+                            </Form.Group>
+
+                            <Form.Group as={Col} >
+                            <Form.Label>Extreme temperature (°C)</Form.Label>
+                            <Form.Control type={"number"} step={0.1}  onChange={(e)=>{this.setState({extremeTemperature:e.target.value})}} />
                             </Form.Group>
                 </Form.Row>
 
+                <Form.Row>
+                
+                            <Form.Group as={Col} >
+                            <Form.Label>Maximum flow (Nm3/hr)</Form.Label>
+                            <Form.Control type={"number"} step={0.1}  onChange={(e)=>{this.setState({maximumFlow:e.target.value})}} />
+                            </Form.Group>
 
-            </Form> 
+                            <Form.Group as={Col} >
+                            <Form.Label>Volume stored(Ltrs / Cyls.)</Form.Label>
+                            <Form.Control type={"number"} step={0.1}  onChange={(e)=>{this.setState({volumeStored:e.target.value})}} />
+                            </Form.Group>
+
+                            <Form.Group as={Col} >
+                            <Form.Label>Characteristics(Purity...)</Form.Label>
+                            <Form.Control type={"number"} step={0.1}  onChange={(e)=>{this.setState({Characteristics:e.target.value})}} />
+                            </Form.Group>
+
+                </Form.Row>
+
+                <Form.Row>
+                            <Col md={4}>
+                            <Card border="secondary">
+                            <Form.Group as={Col} >
+                            <Form.Label>Natures</Form.Label>
+                            <Form.Control as="textarea" rows="1" 
+                            onChange={(e)=>{this.setState({fluidOrProduct:e.target.value})
+                            console.log(e.target.value)}}/>
+                            </Form.Group>
+                            
+                            <Form.Group as={Col} >
+                            <Form.Control as="textarea" rows="1" 
+                            onChange={(e)=>{this.setState({fluidOrProduct:e.target.value})
+                            console.log(e.target.value)}}/>
+                            </Form.Group>
+
+                            <Form.Group as={Col} >
+                            <Form.Control as="textarea" rows="1" 
+                            onChange={(e)=>{this.setState({fluidOrProduct:e.target.value})
+                            console.log(e.target.value)}}/>
+                            </Form.Group>
+                            </Card>
+                            </Col>          
+                </Form.Row>
+
+
+            </Form>
+            </Col>
             </React.Fragment>
         )
       }
